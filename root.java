07c06d0cb294d62bea54332444d56d0f423bcb7e
root <root@acer.(none)>
2012-07-29 20:04:30 +0530

    Square root of 5

---
+++ root.java
@@ -0,0 +1,13 @@
+/*Square root of 5 */
+
+import java.lang.Math;
+
+class sqrt
+{
+ public static void main(String args[])
+ {
+	double x=5,y;
+	y =Math.sqrt(x);
+	System.out.println("The squre root of 5 is : "+y);
+ }
+}
