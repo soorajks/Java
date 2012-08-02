2efa1100670e15b00d6df379cfa2c5156727632b
sooraj <soorajsooriadas@gmail.com>
2012-08-02 16:25:43 +0530

    Floyds Triangle

---
+++ floydstria.java
@@ -0,0 +1,16 @@
+class floydstria
+{
+ public static void main(String args[])
+ {
+	int i,j,count=1;
+	for(i=0;i<13;i++)
+	{
+	 for(j=0;j<=i;j++)
+ 	  {
+		System.out.print("  "+count);
+		count++;
+	  }
+ 	 System.out.print("\n");
+	}
+ }
+}
